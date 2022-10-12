16f4b6c2234afdcc0a213cdf843925598d41928b
Tuzi06 <damienwang21@gmail.com>
2022-10-11 22:09:35 -0700

    fff

---
+++ main.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io.wavfile import read
+from scipy.fft import fft,fftfreq
+import cv2
+
+from moviepy.editor import *
+audioclip = AudioFileClip("data/video.mp4")
+audioclip = AudioFileClip("data/audio.avi")
+
+
+video = cv2.VideoCapture("data/video.mp4")
+fps = video.get(cv2.CAP_PROP_FPS)     
+frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+length = int(frame_count//fps) # seconds
+sampleRate,audio = read('data/audio.wav')
+print(length,sampleRate)
+audio =  np.array([s[0] for s in audio])
+norm = np.int16((audio / audio.max()) * 32767)
+
+x = fft(audio[:length*sampleRate])
+# np.savetxt('data.csv',x,delimiter=',')
+y = fftfreq(length*sampleRate,1/sampleRate)
+print('audio',len(audio),'x',len(x),'\n','y',len(y))
+plt.rcParams["figure.autolayout"] = True
+# plt.plot(x,np.abs(y),'r')
+plt.plot(np.abs(y),np.abs(x))
+plt.show()
